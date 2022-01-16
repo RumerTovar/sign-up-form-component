6e328bcd0601abcd492ccb4de4df9da502b9c74b
Rumer Tovar <rumertovar7474@gmail.com>
2022-01-16 00:19:33 -0500

    fixing google translate bug

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -4,8 +4,8 @@ const Offers = () => {
  return (
   <>
    <p className='Offers'>
-    <span className='OfferSpan'>Try it free 7 days</span> then $20/mo.
-    thereafter
+    <span className='OfferSpan'>Try it free 7 days</span>
+    <span>&nbsp;then $20/mo. thereafter&nbsp;</span>
    </p>
   </>
  );
